fe421388e11a59a8e4844523172de86f5558fbd4
Maike Silva <mikemendessilv@gmail.com>
2023-02-12 18:53:07 -0400

    Botão (sem animação)

--- views/index.php
+++ views/index.php
@@ -1,2 +1,10 @@
 
 <h1 class='titulo'>Lista de Tarefas</h1>
+
+<h2 class='subtitulo'>
+    Diga adeus a desorganização! Crie aqui suas tarefas e de um check conforme for cumprindo as.
+</h2>
+
+<button class='butao nova-tarefa'>
+    Nova Tarefa
+</button>
