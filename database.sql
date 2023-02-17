cdf036806e4d87c486a3095fdffcbcc9af35a258
Maike Silva <mikemendessilv@gmail.com>
2023-02-16 20:12:12 -0400

    Atualizando BD

--- database.sql
+++ database.sql
@@ -4,6 +4,14 @@ CREATE TABLE tarefa(
 
 	id SERIAL PRIMARY KEY,
 	descricao varchar(25) NOT NULL,
-	prazo date NOT NULL
+	prazo date NOT NULL,
+    id_categoria int NOT NULL
 	
+);
+
+CREATE TABLE categoria(
+
+    id SERIAL PRIMARY KEY,
+    descricao varchar(25) NOT NULL
+
 );
