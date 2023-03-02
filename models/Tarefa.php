1edc7b2c4571c037f1131ec2ad8ef3bb29d16387
Maike Silva <mikemendessilv@gmail.com>
2023-03-02 19:03:08 -0400

    Adição order by
    Priorizar as tarefas mais próximas

--- models/Tarefa.php
+++ models/Tarefa.php
@@ -4,7 +4,7 @@
         protected $ordem="id";
 
         public function getByIdCategoria($id_categoria){
-            $sql = "SELECT * FROM $this->tabela WHERE id_categoria = :id_categoria";
+            $sql = "SELECT * FROM $this->tabela WHERE id_categoria = :id_categoria ORDER BY prazo";
             $sentenca = $this->conexao->prepare($sql);
             $sentenca->bindParam(":id_categoria", $id_categoria);
             $sentenca->execute();
